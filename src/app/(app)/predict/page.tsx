f1b5ec6b3db064f4a5488728dcbc96f5a03d7cef
Jim Poole <jimpoole1975@gmail.com>
2025-10-15 20:31:01 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/(app)/predict/page.tsx
+++ src/app/(app)/predict/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, useFieldArray } from 'react-hook-form';
 import * as z from 'zod';
