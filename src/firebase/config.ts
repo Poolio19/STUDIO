f5299054e960cfd13eb2b8152ae91c95231a06bb
Jim Poole <jimpoole1975@gmail.com>
2025-11-18 22:38:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2138583336-cec5d",
+  "appId": "1:806341273085:web:7a57a00f124a54d3172594",
+  "apiKey": "AIzaSyCPf0BmnlKp6ytAzkvvRU-imVM95TXLKmI",
+  "authDomain": "studio-2138583336-cec5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "806341273085"
+};
