2915f62f67f56f716e0b01b3dd879a4c1b1eff60
Jim Poole <jimpoole1975@gmail.com>
2025-11-18 20:07:35 +0000

    I think team name column is too wide and crashes in to main matrix area.

--- src/app/(app)/consensus/page.tsx
+++ src/app/(app)/consensus/page.tsx
@@ -92,7 +92,7 @@ export default function ConsensusPage() {
                 <TableRow>
                   <TableHead className="sticky left-0 z-20 w-[50px] bg-card text-center">Pos</TableHead>
                   <TableHead className="sticky left-[50px] z-20 w-[48px] bg-card"></TableHead>
-                  <TableHead className="sticky left-[98px] z-10 w-[240px] bg-card">Team</TableHead>
+                  <TableHead className="sticky left-[98px] z-10 bg-card">Team</TableHead>
                   {positions.map((pos) => (
                     <TableHead key={pos} className="w-[60px] text-center">{pos}</TableHead>
                   ))}
