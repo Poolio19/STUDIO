b6b1c0e90502288faf0ee9b96188e92687d4b8d6
Jim Poole <jimpoole1975@gmail.com>
2025-11-18 19:59:49 +0000

    increase font size back to 11

--- src/components/charts/player-rank-chart.tsx
+++ src/components/charts/player-rank-chart.tsx
@@ -96,15 +96,14 @@ export function PlayerRankChart({ chartData, yAxisDomain, sortedUsers }: PlayerR
         </ResponsiveContainer>
       </ChartContainer>
        <ul
-        className="absolute flex h-full flex-col justify-between text-xs"
+        className="absolute flex h-full flex-col justify-between"
         style={{
           right: 0,
           top: '-25px',
           bottom: '20px',
           width: '130px',
           paddingLeft: '1rem',
-          transform: 'scale(0.9)',
-          transformOrigin: 'top right',
+          fontSize: '11px',
         }}
       >
         <p className="font-medium mb-2">Player, Rank</p>
