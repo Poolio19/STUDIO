5a413609a34be035b9eb9f456a51e56a3544c2bc
Jim Poole <jimpoole1975@gmail.com>
2025-12-15 13:27:01 +0000

    Keep going, try another method.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,7 @@
 import { AppLayout } from '@/components/layout/app-layout';
-import { FirebaseClientProvider } from '@/firebase';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <FirebaseClientProvider>
       <AppLayout>{children}</AppLayout>
-    </FirebaseClientProvider>
   );
 }
