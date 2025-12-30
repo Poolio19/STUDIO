35cafede0127ed38723412e5898dffaa784a4d8f
Jim Poole <jimpoole1975@gmail.com>
2025-12-30 18:05:14 +0000

    How will i know if / when the bypass test is running?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-2138583336-cec5d",
   "appId": "1:806341273085:web:7a57a00f124a54d3172594",
+  "storageBucket": "studio-2138583336-cec5d.appspot.com",
   "apiKey": "AIzaSyCPf0BmnlKp6ytAzkvvRU-imVM95TXLKmI",
   "authDomain": "studio-2138583336-cec5d.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "806341273085"
 };
