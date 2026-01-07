b08073e255feac702c4262246db670c8c7cde317
Jim Poole <jimpoole1975@gmail.com>
2026-01-07 00:06:18 +0000

    Text should be aligned mid-left of the rotated cell. (which is bottom ce

--- src/app/(app)/stats/page.tsx
+++ src/app/(app)/stats/page.tsx
@@ -85,14 +85,14 @@ export default function StatsPage() {
                                     </div>
                                 </TableHead>
                                 <TableHead className="text-center p-0 w-[40px] border-l border-dashed border-border">
-                                    <div className="[writing-mode:vertical-rl] transform-gpu rotate-180 whitespace-nowrap font-bold h-[92px] w-full flex items-end">{/* bottom-left */}
+                                    <div className="[writing-mode:vertical-rl] transform-gpu rotate-180 whitespace-nowrap font-bold h-[92px] w-full flex items-end justify-center">
                                         TOTAL
                                     </div>
                                 </TableHead>
                                 {sortedTeams.map((team) => {
                                     return (
                                     <TableHead key={team.id} className="text-left p-0 w-[40px] border-l border-dashed border-border">
-                                        <div className="[writing-mode:vertical-rl] transform-gpu rotate-180 whitespace-nowrap font-medium h-[92px] w-full flex items-end">{/* bottom-left */}
+                                        <div className="[writing-mode:vertical-rl] transform-gpu rotate-180 whitespace-nowrap font-medium h-[92px] w-full flex items-end justify-center">
                                             {team.name}
                                         </div>
                                     </TableHead>
