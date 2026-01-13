10da7989ca8b546f2003e8cd2841f766c9e7c5ca
Jim Poole <jimpoole1975@gmail.com>
2026-01-13 15:50:11 +0000

    Keep trying to update the json file - we must make this approach work.

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -3,9 +3,7 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/update-all-data-flow.ts';
-import '@/ai/flows/reimport-fixtures-flow.ts';
 import '@/ai/flows/import-past-fixtures-flow.ts';
+import '@/ai/flows/reimport-fixtures-flow.ts';
 import '@/ai/flows/test-db-write-flow.ts';
 import '@/ai/flows/update-match-results-flow.ts';
-
