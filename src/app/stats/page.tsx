07283c729a7b83fcbef329bc181a1095cb744be1
Jim Poole <jimpoole1975@gmail.com>
2026-01-20 22:38:47 +0000

    Justify start appears correct in terms of vertical alignment.

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -100,7 +100,7 @@ export default function StatsPage() {
                                     return (
                                     <TableHead key={team.id} className="text-left p-0 border-l border-dashed border-border w-[40px]">
                                         <div
-                                            className="[writing-mode:vertical-rl] transform-gpu rotate-180 flex justify-center items-start font-medium h-full w-full pb-1 whitespace-nowrap"
+                                            className="[writing-mode:vertical-rl] transform-gpu rotate-180 flex justify-end items-center font-medium h-full w-full pb-1 whitespace-nowrap"
                                         >
                                             {team.name}
                                         </div>
